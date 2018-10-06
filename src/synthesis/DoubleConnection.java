73f36beab6dec02a671778d6ac48bb805a4aa882
samuelhklumpers <samuelhklumpers@gmail.com>
2018-10-06 20:03:34 +0200

    First upload, base code

---
+++ src/synthesis/DoubleConnection.java
@@ -0,0 +1,49 @@
+package synthesis;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.Arrays;
+
+public class DoubleConnection extends Connection {
+    private double[] buff;
+
+    DoubleConnection() {
+        this.buff = new double[1024];
+    }
+
+    private void resize() {
+        double[] newBuff = new double[2 * buff.length];
+
+        System.arraycopy(buff, 0, newBuff, 0, buff.length);
+        Arrays.fill(newBuff, buff.length, 2 * buff.length, Double.NaN);
+
+        buff = newBuff;
+    }
+
+    @Override
+    public double getSignal(long t) throws OperationNotSupportedException {
+        if (!isAvailable(t))
+        {
+            parent.process(t, this);
+        }
+
+        return buff[(int)t];
+    }
+
+    private boolean isAvailable(long t) {
+        return isInBuffer(t) && !Double.isNaN(buff[(int)t]);
+    }
+
+    private boolean isInBuffer(long t) {
+        return 0 <= t && t < buff.length;
+    }
+
+    @Override
+    public void setSignal(long t, double value) {
+        if (!isInBuffer(t))
+        {
+            resize();
+        }
+
+        buff[(int)t] = value;
+    }
+}
